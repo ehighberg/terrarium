69df64d7b5286c891ea9564f26348d410e8b6252
Errol Highberg <errol.highberg@gmail.com>
2020-03-07 10:47:13 -0500

    experiment CRUD skeletonized

--- server/app/controllers/experiment_controller.rb
+++ server/app/controllers/experiment_controller.rb
@@ -1,3 +1,50 @@
 class ExperimentController < ApplicationController
-  
+  before_action, :set_user, only: [:create, :destroy]
+  before_action, :set_experiment, only: [:destroy]
+  before_action, :authorize_request, except: [:index, :show]
+
+  def index
+    @experiments = Experiment.all
+    render json: @experiments
+  end
+
+  def show
+    render json: @experiment
+  end
+
+  def create
+    @experiment = Experiment.new(experiment_params)
+
+    if @experiment.save
+      render json: @experiment, status: :created, location: @experiment
+    else
+      render json: @experiment.errors, status: :unprocessable_entity
+    end
+  end
+
+  def update
+    if @experiment.update(experiment_params)
+      render json: @experiment
+    else
+      render json: @experiment.errors, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @experiment.destroy
+  end
+
+  private
+
+  def set_experiment
+    @experiment = Experiment.find(params[:id])
+  end
+
+  def experiment_params
+    params.permit(:time_start, :time_end, :target, :metric, :final_score, :history, :user_id)
+  end
+
+  def model_params
+    params.permit(:model_type, :model_params)
+  end
 end
