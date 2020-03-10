a9cef415b20e5484d906ce5b0a8fc687f9bab74b
Errol Highberg <errol.highberg@gmail.com>
2020-03-10 10:39:40 -0400

    data preprocessed

---
+++ ml/src/features/build_features.py
@@ -0,0 +1,24 @@
+# %%
+import pandas as pd
+
+
+# %%
+# Import dataset
+
+def import_dataset(filepath, header=None):
+    return pd.read_excel(filepath, header=header)
+
+CCPP_raw = import_dataset('./ml/data/raw/CCPP/Folds5x2_pp.xlsx', 0)
+
+
+# %%
+# View imported dataset
+
+print(CCPP_raw.sample(10))
+print(CCPP_raw.dtypes)
+
+
+# %%
+# Export dataset
+
+CCPP_raw.to_csv('./ml/data/processed/ccpp.csv')
