8da08c6fab2d0b0768cde39a91b658e83bf1d631
Errol Highberg <errol.highberg@gmail.com>
2020-03-12 14:11:29 -0400

    Results component complete

--- client/src/component/shared/Results.jsx
+++ client/src/component/shared/Results.jsx
@@ -4,7 +4,20 @@ const Results = props => {
 
   return (
     <div className='results-container'>
-
+      <div className='results-visual-container'>
+        <img alt='results visualization' src='#' />
+      </div>
+      <div className='results-summary-container'>
+        <p className='results-summary-title'>Results</p>
+        <div className='results-summary-items'>
+          {props.experiment.final_score && (
+            <div className='results-final-score'>
+              <p>Final R-Squared on test data: </p>
+              <p>{props.experiment.final_score}</p>
+            </div>
+          )}
+        </div>
+      </div>
     </div>
   )
 }
